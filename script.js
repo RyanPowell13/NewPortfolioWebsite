0d4ab61da1e81af5effbefd6a9198afe617a9352
RYAN-DESKTOP\Ryan <ryanjamespowell13@gmail.com>
2021-12-02 18:07:30 -0600

    Added some more animation

---
+++ script.js
@@ -0,0 +1,8 @@
+var contact = document.getElementById("contact-button")
+
+contact.onclick = function(){
+
+    document.getElementById("contact-button").style.color = "purple";
+    return false;
+    
+}

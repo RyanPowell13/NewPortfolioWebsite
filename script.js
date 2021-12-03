9e2be5d09e7929a473c639a95679ac938849582e
RYAN-DESKTOP\Ryan <ryanjamespowell13@gmail.com>
2021-12-03 14:56:44 -0600

    more nav updates

--- script.js
+++ script.js
@@ -1,17 +1,58 @@
+var home = document.getElementById("home-button")
 var contact = document.getElementById("contact-button")
 var subtext = document.getElementById("subtext")
 var form = document.getElementById("form")
-var navbar = document.querySelectorAll(".active")
 
+var delayInMilliseconds = 7000;
+var onLoad =  document.querySelectorAll(".onLoad")
+setTimeout(function() {
+    [].forEach.call(onLoad, function(items) {
+        items.classList.remove("onLoad")
+    });
+      }, delayInMilliseconds);
+
+
+      
 contact.onclick = function(){
 
+    var navbar = document.querySelectorAll(".active")
+    var delayInMilliseconds = 1500;
+
+    document.getElementById("navbar").classList.add("navigation");
     document.getElementById("h1").innerHTML = "Contact Me.";
     subtext.style.display = "none";
-    form.style.display = "inline";
+    form.style.display = "block";
     [].forEach.call(navbar, function(items) {
         items.classList.remove("active")
     });
     contact.classList.add("active")
+
+    setTimeout(function() {
+        document.getElementById("navbar").classList.remove("navigation");
+      }, delayInMilliseconds);
+
+    return false;
+
+}
+
+home.onclick = function(){
+
+    var navbar = document.querySelectorAll(".active")
+    var delayInMilliseconds = 1500;
+
+    document.getElementById("navbar").classList.add("navigation");
+    document.getElementById("h1").innerHTML = "Ryan Powell.";
+    subtext.style.display = "inline";
+    form.style.display = "none";
+    [].forEach.call(navbar, function(items) {
+        items.classList.remove("active")
+    });
+    home.classList.add("active")
+
+    setTimeout(function() {
+        document.getElementById("navbar").classList.remove("navigation");
+      }, delayInMilliseconds);
+
     return false;
     
 }

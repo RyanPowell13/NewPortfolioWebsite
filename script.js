964f62a8efd7596748b6342f0d9b9ca7b015c4df
RYAN-DESKTOP\Ryan <ryanjamespowell13@gmail.com>
2021-12-03 11:53:41 -0600

    added Contact Me functionality.

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 var contact = document.getElementById("contact-button")
+var subtext = document.getElementById("subtext")
+var form = document.getElementById("form")
+var navbar = document.querySelectorAll(".active")
 
 contact.onclick = function(){
 
-    document.getElementById("contact-button").style.color = "purple";
+    document.getElementById("h1").innerHTML = "Contact Me.";
+    subtext.style.display = "none";
+    form.style.display = "inline";
+    [].forEach.call(navbar, function(items) {
+        items.classList.remove("active")
+    });
+    contact.classList.add("active")
     return false;
     
 }

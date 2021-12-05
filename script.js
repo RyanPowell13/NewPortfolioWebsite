4ef16872346a9b04665b5d6194f3f33d674e84e4
RYAN-DESKTOP\Ryan <ryanjamespowell13@gmail.com>
2021-12-04 18:50:51 -0600

    made resume page

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 var home = document.getElementById("home-button")
+var resume = document.getElementById("resume-button")
 var contact = document.getElementById("contact-button")
 var subtext = document.getElementById("subtext")
 var form = document.getElementById("form")
+var resumeText = document.getElementById("resumeText")
 
 var delayInMilliseconds = 7000;
 var onLoad =  document.querySelectorAll(".onLoad")
@@ -21,6 +23,7 @@ contact.onclick = function(){
     document.getElementById("navbar").classList.add("navigation");
     document.getElementById("h1").innerHTML = "Contact Me.";
     subtext.style.display = "none";
+    resumeText.style.display = "none";
     form.style.display = "block";
     [].forEach.call(navbar, function(items) {
         items.classList.remove("active")
@@ -44,10 +47,34 @@ home.onclick = function(){
     document.getElementById("h1").innerHTML = "Ryan Powell.";
     subtext.style.display = "inline";
     form.style.display = "none";
+    resumeText.style.display = "none";
     [].forEach.call(navbar, function(items) {
         items.classList.remove("active")
     });
     home.classList.add("active")
+
+    setTimeout(function() {
+        document.getElementById("navbar").classList.remove("navigation");
+      }, delayInMilliseconds);
+
+    return false;
+    
+}
+
+resume.onclick = function(){
+
+    var navbar = document.querySelectorAll(".active")
+    var delayInMilliseconds = 1500;
+
+    document.getElementById("navbar").classList.add("navigation");
+    document.getElementById("h1").innerHTML = "Resume.";
+    subtext.style.display = "none";
+    form.style.display = "none";
+    resumeText.style.display = "inline";
+    [].forEach.call(navbar, function(items) {
+        items.classList.remove("active")
+    });
+    resume.classList.add("active")
 
     setTimeout(function() {
         document.getElementById("navbar").classList.remove("navigation");
